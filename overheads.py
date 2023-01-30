a405bb4487590a33ae8c8b335c644e34b69b451c
raphaelyeo <raphaeltheyeo@gmail.com>
2023-01-31 02:57:18 +0800

    changed overhead code

--- overheads.py
+++ overheads.py
@@ -7,7 +7,6 @@ def overhead():
     - This function finds the highest overhead type
     - No parameters required.
     """
-
     # Instantiate file path to current working directory
     fp_readoverhead = Path.cwd()/"project_group2"/"csv_reports"/"Overheads.csv"
     fp_write = Path.cwd()/"summary_report.txt"
@@ -24,31 +23,14 @@ def overhead():
         percentage = []
         for row in reader:
             overhead_type.append(row[0])
-            percentage.append(row[1])
+            percentage.append(float(row[1]))
         
         # Use max() function and set key=float to find highest percentage
         max_percentage = max(percentage, key=float)
-        # Use if statements to assign the highest to max_overhead_type variable
-        if max_percentage == percentage[0]:
-            max_overhead_type = overhead_type[0]
-        if max_percentage == percentage[1]:
-            max_overhead_type = overhead_type[1]
-        if max_percentage == percentage[2]:
-            max_overhead_type = overhead_type[2]
-        if max_percentage == percentage[3]:
-            max_overhead_type = overhead_type[3]
-        if max_percentage == percentage[4]:
-            max_overhead_type = overhead_type[4]
-        if max_percentage == percentage[5]:
-            max_overhead_type = overhead_type[5]
-        if max_percentage == percentage[6]:
-            max_overhead_type = overhead_type[6]
-        if max_percentage == percentage[7]:
-            max_overhead_type = overhead_type[7]
-        if max_percentage == percentage[8]:
-            max_overhead_type = overhead_type[8]
-        if max_percentage == percentage[9]:
-            max_overhead_type = overhead_type[9]
+        
+        # Find the index of the highest percentage from percentage list
+        # Assign max_overhead_type variable to the highest percentage's overhead type by using the same index
+        max_overhead_type = overhead_type[percentage.index(max_percentage)]
 
     # Use mode "w" to write data into file because overheads goes first    
     with fp_write.open(mode="w", encoding="UTF-8") as file2:
