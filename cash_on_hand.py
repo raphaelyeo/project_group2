954f09c3d88298f380ad56249cca9df9f102f97b
AlvinLim959 <123544324+AlvinLim959@users.noreply.github.com>
2023-02-01 12:50:25 +0800

    Update cash_on_hand.py

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -1,13 +1,13 @@
 from pathlib import Path
 import csv
 
-# Define function to calculate cash_difference
 def cash_hand():
     """
-    - This function calculates cash on hanh_difference based on cash amounts over multiple days
-    - No parameters required
-    """
+    This function calculates cash on hand difference based on cash amounts over multiple days.
 
+    Returns:
+        A string indicating either a cash surplus or deficit and its respective day and amount.
+    """
     # Instantiate file path to current working directory
     fp_readcash = Path.cwd()/"project_group2"/"csv_reports"/"Cash on Hand.csv"
     fp_write = Path.cwd()/"summary_report.txt"
@@ -15,21 +15,21 @@ def cash_hand():
     # Use mode "r" to read file
     with fp_readcash.open(mode="r", encoding="UTF8") as file:
         reader = csv.reader(file)
-
+        
         # Skip header
         next(reader)
-
+        
         # Create empty list to store cash and day values
         cash = []
         dayc = []
 
-    # Convert day and cash to float/int respectively and append them to their empty list 
+        # Convert day and cash to float/int respectively and append them to their empty list 
         for row in reader:
             row[1] = int(row[1])
             row[0] = float(row[0])
             cash.append(row[1])
             dayc.append(row[0])
-
+        
         # Initialise counter to 0
         i = 0 
 
@@ -46,9 +46,10 @@ def cash_hand():
                 # If cash_difference is positive, write its respective data into the .txt file
                 if cash_difference > 0:
                     file2.writelines(f"\n[CASH DEFICIT] DAY: {dayc[values+1]}, AMOUNT: USD{cash_difference} ")
-
+                    
                     # Add 1 to counter
                     i +=1 
+                    
         with fp_write.open(mode="a", encoding="UTF-8") as file2:
 
             # If counter is 0, all cash differences are negative
